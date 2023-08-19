e2f787994cc46fa01ed69404b13c6d58cfe2d2fe
nextor <nextor@gmail.com>
2023-08-19 10:10:04 -0700

    Adding styles

--- index.php
+++ index.php
@@ -1,31 +1,56 @@
-<a href="/phpmyadmin">PhpMyadmin</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+	<div>
+	</div>
+	<div class="m-3 card">
+	<ul class="list-group">
+		<li class="list-group-item">
+			<a href="/phpmyadmin">PhpMyadmin</a>
+		</li>
+	<?php
+		$servername = "127.0.0.1";
+		$username = "root";
+		$password = "asdf";
+		$dbname = "_registro_pos";
+		
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) 
+		{
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT * FROM store WHERE status = 'ACTIVE' OR status = 'UNLISTED' ORDER BY storename";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0) 
+		{
+			// output data of each row
+			while($row = $result->fetch_object()) 
+			{
+				?>
+					<li class="list-group-item">
+						<?php echo '<a href="http://127.0.0.'.$row->id.':4000">'.$row->storename.'</a>';?>
+					</li>
+				<?php
+			}
+		}
+		else 
+		{
+			echo "0 results";
+		}
+		$conn->close();
+	?>
+	</ul>
+	</div>
+</body>
+</html>
 
-<?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "asdf";
-$dbname = "_registro_pos";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM store WHERE status = 'ACTIVE' OR status = 'UNLISTED' ORDER BY storename";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) 
-{
-	// output data of each row
-	while($row = $result->fetch_object()) 
-	{
-		echo '<divv><a href="http://127.0.0.' . $row->id.':4000" style="margin-top:20px;font-size:20px">' . $row->storename.'</a><div>';
-	}
-}
-else 
-{
-	echo "0 results";
-}
-$conn->close();
